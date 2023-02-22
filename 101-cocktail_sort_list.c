73bcc3dfe617bb49045ef454528634795eb81930
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-22 17:46:05 +0000

    fix

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -3,7 +3,7 @@
  * cocktail_sort_list - sorts an array of integers in ascending
  * order using cocktail sort algorithm
  *
- * @list: list to sort
+ * @list: list to be sorted
  */
 
 void cocktail_sort_list(listint_t **list)
@@ -18,7 +18,7 @@ void cocktail_sort_list(listint_t **list)
 		while (temp->next)
 		{
 			if (temp->n > temp->next->n)
-				swap_listint_t(temp->next, temp, list);
+				swap_list_nodes(temp->next, temp, list);
 			else
 				temp = temp->next;
 		}
@@ -26,7 +26,7 @@ void cocktail_sort_list(listint_t **list)
 		while (temp->prev != head)
 		{
 			if (temp->n < temp->prev->n)
-				swap_listint_t(temp, temp->prev, list);
+				swap_list_nodes(temp, temp->prev, list);
 			else
 				temp = temp->prev;
 		}
@@ -35,21 +35,27 @@ void cocktail_sort_list(listint_t **list)
 }
 
 /**
- * swap_node - swap adjacent nodes
+ * swap_list_nodes - swap adjacent nodes
  *
- * @x: first node
- * @y: second node
+ * @a: first node
+ * @b: second node
+ * @list: list
  */
-
-void swap_node(listint_t *x, listint_t *y)
+void swap_list_nodes(listint_t *a, listint_t *b, listint_t **list)
 {
-	x->next = y->next;
-	if (y->next)
-		y->next->prev = x;
-	y->next = x;
+	listint_t *c, *d;
 
-	y->prev = x->prev;
-	if (x->prev)
-		x->prev->next = y;
-	x->prev = y;
+	c = a->next;
+	d = b->prev;
+	if (c)
+		c->prev = b;
+	if (d)
+		d->next = a;
+	a->prev = d;
+	b->next = c;
+	a->next = b;
+	b->prev = a;
+	if (*list == b)
+		*list = a;
+	print_list(*list);
 }
