8f691fd81774abf1af79b5cd800d752805710128
MOhammed Zakaria <kokoriquaye@gmail.com>
2023-02-22 17:47:15 +0000

    header

--- sort.h
+++ sort.h
@@ -35,5 +35,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void swap_listint_t(listint_t *a, listint_t *b, listint_t **list);
+void swap_list_nodes(listint_t *a, listint_t *b, listint_t **list);
 #endif
